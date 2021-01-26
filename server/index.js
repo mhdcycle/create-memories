e5760350fefbef78d335693623cf68829b630185
Mohammed Cyclewala <mohammedcyclewala.cm@gmail.com>
2021-01-25 22:35:05 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+
+app.get('/', (req, res) => {
+    res.send("Hello to Memories Post");
+})
+// const CONNECTION_URL = 'mongodb+srv://mhdcycle:12345@practice.jp4hw.mongodb.net/Post?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, ()=>console.log(`Server is running on ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
